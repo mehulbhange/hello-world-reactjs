7760bb124f8fb615980ac7d5068d688e5f1b96cf
mehulbhange <bhangemehul@gmail.com>
2022-11-17 12:18:46 +0530

    ADDED:[Mehul] on click event to launch bridgelabz site in new tab

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 import logo from './assets/img/BL_logo.png'
 
 class App extends React.Component {
+  url = "https://www.bridgelabz.com/";
   constructor(){
     super();
     this.state ={
       title:'Hello from Bridgelabz'
     }
   }
+
+  onClick = (event) =>{
+      console.log("Button clicked!");
+      window.open(this.url,"_blank");
+  }
+
   render(){
     return (
       <div className="App">
         <h1> {this.state.title} </h1>
-        <img src={logo}  alt="Bridgelabz logo" />
+        <img src={logo} onClick={this.onClick}  alt="Bridgelabz logo" />
       </div>
     );
   }
